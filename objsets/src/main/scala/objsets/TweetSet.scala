8ab0f8c0d070069e4d75005bf14057b563a0840b
Om Prakash C <comprakash@gmail.com>
2017-01-23 10:42:41 +0530

    Assignment 3 work in progress

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,8 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet =
+      filterAcc(p, new Empty)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,7 +55,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+    def union(that: TweetSet): TweetSet
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -65,7 +66,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def mostRetweeted: Tweet = ???
+    def mostRetweeted: Tweet
   
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
@@ -107,7 +108,11 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = Empty.this
+
+  def union(that: TweetSet): TweetSet = that
+
+  def mostRetweeted: Tweet =
   
   /**
    * The following methods are already implemented
@@ -124,8 +129,20 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet =
+      if (p(elem)) filterAcc(p, acc.incl(elem))
+      else acc
+
+  def union(that: TweetSet): TweetSet =
+    if (that.contains(elem)) left.union(that).union(right.union(that))
+    else left.union(that.incl(elem)).union(right.union(that.incl(elem)))
+
+  def mostRetweeted: Tweet =
+    mostRetweeted(0)
+
+  def mostRetweeted(acc: Int): Tweet =
+    if (acc > elem.retweets) left.mostRetweeted(acc)
+    else right.mostRetweeted(acc)
     
   /**
    * The following methods are already implemented
