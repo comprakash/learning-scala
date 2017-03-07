345ff7f76f3bb0feed52477535be13a4510ecb2d
Om Prakash C <comprakash@gmail.com>
2017-03-07 14:15:39 +0530

    Added Time Converter in Time Zone Utilities

--- time_utilities/src/main/scala/TimeZoneUtil.scala
+++ time_utilities/src/main/scala/TimeZoneUtil.scala
@@ -1,8 +1,26 @@
 /**
   * Created by Om Praksah C on 2/16/2017.
   */
+import java.time._
 
 object TimeZoneUtil extends App {
-  val timeZoneList = java.time.ZoneId.getAvailableZoneIds()
-  timeZoneList.forEach(timeZone => println(timeZone + "," + java.time.ZonedDateTime.now(java.time.ZoneId.of(timeZone))))
+  val timeZoneList = ZoneId.getAvailableZoneIds()
+  timeZoneList.forEach(timeZone => println(timeZone + "," + java.time.ZonedDateTime.now(ZoneId.of(timeZone))))
+}
+
+object TimeConverter extends App {
+  val isoTimeWithSeconds = "2017-07-14T02:40:00Z"
+  val isoTimeWithMilliSeconds = "2017-07-14T02:40:00.123Z"
+  val isoTimeWithMicroSeconds = "2017-07-14T02:40:00.123456Z"
+  val isoTimeWithNanoSeconds = "2017-07-14T02:40:00.123456789Z"
+  val instant = Instant.parse(isoTimeWithNanoSeconds)
+  println(instant)
+  println(instant.toString)
+  println(instant.getEpochSecond())
+  println(instant.toEpochMilli)
+  println(instant.getNano)
+  println(instant.getEpochSecond() + "." + instant.getNano)
+
+  println(instant.plusNanos(2))
+
 }
